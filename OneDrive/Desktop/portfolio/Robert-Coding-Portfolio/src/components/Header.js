bcb5961d140011529084de0b256ac7fa4c202b69
MysteriousRob <robertjohngasior@gmail.com>
2024-04-15 12:06:42 +0200

    added header

---
+++ OneDrive/Desktop/portfolio/Robert-Coding-Portfolio/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../assets/css/Header.css';
+
+function Header() {
+  <>
+    <div className="header-body">
+        
+    </div>
+  </>
+}
+export default Header;
