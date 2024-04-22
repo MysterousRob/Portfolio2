9049dcdd62937162ad90be87f185ec63ada93a46
MysteriousRob <robertjohngasior@gmail.com>
2024-04-22 18:28:36 +0200

    making radial progress bar

--- src/Components/Layout/Body/Body.jsx
+++ src/Components/Layout/Body/Body.jsx
@@ -1,11 +1,37 @@
 import React from "react";
-
+import './Body.css';
 
 function Body() {
     return(
         <>
             <div className="home-Body">
-              
+            <section className="image-section">
+        <h2>Image Section</h2>
+        <div className="image-container">
+            <img src="your-image-url.jpg" alt="Your Image" />
+        </div>
+    </section>
+
+    <section className="progress-section">
+        <h2>Progress Bars Section</h2>
+        <div className="progress-container">
+            <div className="progress-item">
+                <div className="progress-bar" style={{ "--value": "70" }} role="progressbar">
+                    <div className="progress-label">
+                        <br />
+                        <br />
+                        <br />
+                        <p className="percentage">
+                            70
+                        </p>
+                        <p className="label">
+                            HTML
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
             </div>
         </>
     )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,14 @@ import '../assets/css/App.css';
 import Body from '../Components/Layout/Body/Body.jsx';
 import Footer from '../Components/Layout/Footer/Footer.jsx';
 import Header from '../Components/Layout/Header/Header.jsx';
+import InnerBody from '../Components/Layout/Body/InnerBody.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
+      <InnerBody />
       <Footer />
     </div>
   );
